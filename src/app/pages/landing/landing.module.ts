861e8a7cc213bc8661d2d85774b2914ec0d89ff6
Tom <tom.lefevrebonzon@gmail.com>
2026-02-03 15:54:05 +0100

    fix(navbar): remove navbar from web landing page, keep only for native app

--- src/app/pages/landing/landing.module.ts
+++ src/app/pages/landing/landing.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { LandingPage } from './landing.page';
-import { NavbarComponent } from '@shared/index';
 
 import { LandingPageRoutingModule } from './landing-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    IonicModule,
-    LandingPageRoutingModule,
-    NavbarComponent,
-  ],
+  imports: [CommonModule, IonicModule, LandingPageRoutingModule],
   declarations: [LandingPage],
 })
 export class LandingPageModule {}
