43d926b94cfbccabbf1574f13a366a48b13586a9
iliesmahoudeau <ilies.mahoudeau@icloud.col>
2026-04-24 12:46:10 +0200

    feat(cyna-app): UserAddress interface and snapshot mapper

--- src/app/core/interfaces/index.ts
+++ src/app/core/interfaces/index.ts
@@ -6,3 +6,4 @@ export * from './payment.interface';
 export * from './order.interface';
 export * from './subscription.interface';
 export * from './license.interface';
+export * from './user-address.interface';
